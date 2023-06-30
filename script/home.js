fbac1d5b6e3c71109ba2b4c89054fb766c6cf46c
Sarah Bourgeois <https://github.com/Sarah2511>
2023-06-30 12:22:52 +0200

    découverte js homepage

--- script/home.js
+++ script/home.js
@@ -1,21 +1,24 @@
-//Initialisation des variables
-const main = document.querySelector("main");
-const sectionCharacter = document.createElement("section");
-sectionCharacter.setAttribute("class", "section");
-main.appendChild(sectionCharacter);
-/*fetch("https://character-database.becode.xyz/characters")
+//initialisation des variables
+
+const body = document.querySelector("body");
+const main = document.createElement("main");
+const footer = document.querySelector("footer");
+body.append(main);
+
+body.insertBefore(main, footer);
+
+fetch("https://character-database.becode.xyz/characters")
 .then(response => response.json())
-.then(data=>{
+.then((data) =>{
     console.log("Vous avez reussi a récupérer les apis.");
     //Boucle permettant de créer des articles
-    data.forEach(ligne => {
+    data.forEach(ligne  => {
         let articleCharacter = document.createElement("article");
-        sectionCharacter.innerText = ligne;
-        sectionCharacter.appendChild(articleCharacter);
-        console.log(articleCharacter);
-
+        main.append(articleCharacter);
+        articleCharacter.innerText = ligne.name;
+        //articleCharacter.innerText = ligne.descritpion
         //aller rechercher les noms
-
+        console.log(ligne);
     });
 })
-.catch(error => console.log("Une erreur c'est produite lors du chargement de la page"))*/
+.catch(error => console.log("Une erreur c'est produite lors du chargement de la page"))
