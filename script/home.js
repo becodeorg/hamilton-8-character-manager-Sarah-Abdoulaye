f24cb6bd2841c2a0c49fcea3756b63d2853eeb3b
Sarah Bourgeois <https://github.com/Sarah2511>
2023-07-04 08:08:13 +0200

    ajout de photos dans les cartes

--- script/home.js
+++ script/home.js
@@ -1,5 +1,4 @@
 //initialisation des variables
-
 const body = document.querySelector("body");
 const main = document.createElement("main");
 const footer = document.querySelector("footer");
@@ -13,34 +12,37 @@ fetch("https://character-database.becode.xyz/characters")
     console.log("Vous avez reussi a récupérer les apis.");
     //Boucle permettant de créer des articles
     data.forEach(ligne  => {
+        // Permet de creer l'article en fonction du nombre d'élément dans l'API
         let articleCharacter = document.createElement("article");
         main.append(articleCharacter);
         
+        // Permet de creer un paragraphe "nom" pour chaque article
         let nom = document.createElement("p");
         nom.className = "nom";
         articleCharacter.append(nom);
         nom.innerText = ligne.name;
 
-        
+        // Permet de creer une div "short description" pour chaque article
         let shortdescription = document.createElement("div");
         shortdescription.className = "shortdescription";
         articleCharacter.append(shortdescription);
         shortdescription.innerText = ligne.shortDescription;
 
-
+        // Permet de creer une div "description" pour chaque article
         let description = document.createElement("div");
         description.className = "description";
         articleCharacter.append(description);
         description.innerHTML = ligne.description;
 
+        // Permet de creer une img "img" pour chaque article
         let img = document.createElement("img");
         articleCharacter.append(img);
+        const imgBase = ligne.image;
+        img.src = 'data:image/png;base64,' + imgBase;
 
-        //articleCharacter.innerHTML = ligne.image
-
-
-        console.log(ligne);
-        console.log(articleCharacter);
+        // Permet d'afficher pour tester. 
+        //console.log(imgBase);
+        console.log(img);
     });
 })
-.catch(error => console.log("Une erreur c'est produite lors du chargement de la page"))
+.catch(error => console.log("Une erreur c'est produite lors du chargement de la page"));
