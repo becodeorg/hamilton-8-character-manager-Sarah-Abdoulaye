ecb10032511d4e6249de89d3bc490a4905e6d576
Sarah Bourgeois <https://github.com/Sarah2511>
2023-07-06 16:08:32 +0200

    part 1 refactor

--- script/create-edit.js
+++ script/create-edit.js
@@ -1,14 +1,13 @@
-// Récupérer l'ID du post à partir des paramètres de l'URL
-let postID = new URLSearchParams(window.location.search).get('id');
+const params = new URLSearchParams(window.location.search);
+const postID = params.get('id');
 
-// Condition : si l'ID récupéré n'est pas null (il possède un ID), alors on récupère les données pour éditer
 if (postID !== null) {
-    // Fonction permettant de récupérer les données à partir de leur ID
-    function renderOneCharacter(data) {   
+    // Fonction pour afficher les données du personnage sur la page
+    const renderOneCharacter = (data) => {   
         document.querySelector('#name').value = data.name;
         document.querySelector('#description').value = data.description;
-        document.querySelector('img')
-    }
+        document.querySelector('img');
+    };
 
     // Récupérer les données du personnage à partir de son ID
     fetch(`https://character-database.becode.xyz/characters/${postID}`)
@@ -17,9 +16,10 @@ if (postID !== null) {
         .catch(error => console.log(error));
 
     // Récupérer les valeurs des champs de texte pour le nom et la description du personnage
-    let name = document.querySelector('#name').value;
-    let description = document.querySelector('#description');
+    const name = document.querySelector('#name').value;
+    const description = document.querySelector('#description').value;
 
+    // Mettre à jour les données du personnage sur le serveur
     (async () => {
         const rawResponse = await fetch(`https://character-database.becode.xyz/characters/${postID}`, {
             method: 'PUT',
@@ -28,61 +28,63 @@ if (postID !== null) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                name: name,
-                description: description,
+                name,
+                description,
                 image: image64
             }) 
         });
-        
+
         const content = await rawResponse.json();
     })();
 }
 
-// Suite de la condition : si l'ID est null (n'existe pas), alors on crée un nouveau personnage
 else {
     let image64;
 
-    // Fonction qui permet d'enregistrer les données apportées par l'utilisateur
-    document.querySelector("#enregistrer").onclick = function(e) {
+    // Fonction pour enregistrer les données du nouveau personnage
+    document.querySelector("#enregistrer").onclick = async function(e) {
         e.preventDefault();
-        
+
         // Convertir l'image en une URL de données (base64)
-        const toDataURL = url => fetch(url)
-            .then(response => response.blob())
-            .then(blob => new Promise((resolve, reject) => {
+        const toDataURL = async (url) => {
+            const response = await fetch(url);
+            const blob = await response.blob();
+            return new Promise((resolve, reject) => {
                 const reader = new FileReader();
                 reader.onloadend = () => resolve(reader.result);
                 reader.onerror = reject;
                 reader.readAsDataURL(blob);
-            }));
+            });
+        };
 
         // Récupérer le nom et la description du personnage à partir des champs de texte
         const name = document.querySelector("#name").value;
-        const description = document.querySelector("#description").value; 
+        const description = document.querySelector("#description").value;
 
-        // Convertir l'image en URL de données (base64) et enregistrer la partie codée en base64
-        toDataURL(document.querySelector('input[type=file]').files[0].name)
-            .then(dataUrl => {
-                console.log(dataUrl);
-                let index = dataUrl.indexOf(',');
-                image64 = dataUrl.substring(index + 1);
-                
-                // Envoyer les données du personnage au serveur pour créer un nouveau personnage
-                fetch("https://character-database.becode.xyz/characters", {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        name: name,
-                        description: description,
-                        image: image64
-                    }) 
-                })
-                .then(response => response.json())
-                .catch(error => console.log(error));
+        try {
+            // Convertir l'image en URL de données (base64)
+            const dataUrl = await toDataURL(document.querySelector('input[type=file]').files[0].name);
+            console.log(dataUrl);
+            const index = dataUrl.indexOf(',');
+            image64 = dataUrl.substring(index + 1);
+
+            // Envoyer les données du personnage au serveur pour créer un nouveau personnage
+            const response = await fetch("https://character-database.becode.xyz/characters", {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name,
+                    description,
+                    image: image64
+                }) 
             });
-    }
+            
+            const content = await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
-
--- script/home.js
+++ script/home.js
@@ -1,142 +1,96 @@
-//initialisation des variables
 const body = document.querySelector("body");
 const main = document.createElement("main");
 const footer = document.querySelector("footer");
-const search= document.querySelector("#search");
+const search = document.querySelector("#search");
 body.append(main);
 body.insertBefore(main, footer);
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Code pour supprimer le personnage choisi
-function deleteButton(ligne, buttondelete){
-    buttondelete.onclick= function(e){
-        console.log(buttondelete);
+// Fonction pour supprimer le personnage choisi
+const deleteButton = (ligne, buttondelete) => {
+    buttondelete.addEventListener("click", async (e) => {
         e.preventDefault();
-        fetch("https://character-database.becode.xyz/characters/" + ligne, {
-            method: "DELETE",
-            //Le header défini que les données qu'on envoi seront en format JSON (metadata, complément d'information à destination de l'API)
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-    }
-}
+        if (confirm("Êtes-vous sûr de vouloir supprimer cette carte ?")) {
+            try {
+                const response = await fetch(`https://character-database.becode.xyz/characters/${ligne}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                if (response.ok) {
+                    console.log("Personnage supprimé avec succès.");
+                } else {
+                    console.error("Erreur lors de la suppression du personnage.");
+                }
+            } catch (error) {
+                console.error("Une erreur s'est produite lors de la suppression du personnage.", error);
+            }
+        }
+    });
+};
 //------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//searchbar
-search.onclick = function(e){
-    let valeur = document.querySelector("#search-bar").value;
-    
-    fetch('https://character-database.becode.xyz/characters?name='+ valeur)
-    .then(response => response.json())
-    .then(data => {
-        console.table(data);
-        e.preventDefault();
-        while (main.firstChild) {
-            main.removeChild(main.firstChild);
+// Gestionnaire d'événement pour la barre de recherche
+search.addEventListener("click", async (e) => {
+    const valeur = document.querySelector("#search-bar").value;
+    try {
+        const response = await fetch(`https://character-database.becode.xyz/characters?name=${valeur}`);
+        if (response.ok) {
+            const data = await response.json();
+            console.table(data);
+            e.preventDefault();
+            main.innerHTML = "";
+            data.forEach(createArticle);
+        } else {
+            console.error("Erreur lors de la recherche des personnages.");
         }
-        for ( element of data) {
-            
-        // Permet de creer l'article en fonction du nombre d'élément dans l'API
-            let articleCharacter = document.createElement("article");
-            main.append(articleCharacter);
-        
-            // Permet de creer une img "img" pour chaque article
-            let img = document.createElement("img");
-            articleCharacter.append(img);
-            img.src = 'data:image/png;base64,' + element.image;
-
-            // Permet de creer un paragraphe "nom" pour chaque article
-            let nom = document.createElement("p");
-            nom.className = "nom";
-            articleCharacter.append(nom);
-            nom.innerText = element.name;
-            
-            // Permet de creer une div "short description" pour chaque article
-            let shortdescription = document.createElement("div");
-            shortdescription.className = "shortdescription";
-            articleCharacter.append(shortdescription);
-            shortdescription.innerText = element.shortDescription;
-        
-            // Permet de creer une div "description" pour chaque article
-            let description = document.createElement("div");
-            description.className = "description";
-            articleCharacter.append(description);
-            description.innerHTML = element.description;
-
-            //permet de creer un bouton pemettant de supprimer une carte
-            let buttondelete = document.createElement("button");
-            articleCharacter.appendChild(buttondelete);
-            buttondelete.className = "buttondelete";
-            buttondelete.innerHTML = "Delete";
-            deleteButton(element.id, buttondelete);
-            
-            // bouton permmettant de modifier une carte
-            let buttonedit = document.createElement("a");
-            articleCharacter.appendChild(buttonedit);
-            buttonedit.setAttribute('href','create.html?id='+data.id)
-            buttonedit.className = "buttonedit";
-            buttonedit.innerHTML = "Edit";
-            }
-    })
-}
+    } catch (error) {
+        console.error("Une erreur s'est produite lors de la recherche des personnages.", error);
+    }
+});
 
 fetch("https://character-database.becode.xyz/characters")
-.then(response => response.json())
-.then((data) =>{
-    
-    console.log("Vous avez reussi a récupérer les API.");
-    //Boucle permettant de créer des articles
-    data.forEach(ligne  => {
-        
-        // Permet de creer l'article en fonction du nombre d'élément dans l'API
-        let articleCharacter = document.createElement("article");
-        main.append(articleCharacter);
-        
-        // Permet de creer une img "img" pour chaque article
-        let img = document.createElement("img");
-        articleCharacter.append(img);
-        const imgBase = ligne.image;
-        img.src = 'data:image/png;base64,' + imgBase;
+    .then(response => response.json())
+    .then((data) => {
+        console.log("Vous avez réussi à récupérer les données de l'API.");
+        data.forEach(createArticle);
+    })
+    .catch(error => console.log("Une erreur s'est produite lors du chargement de la page", error));
 
-         // Permet de creer un paragraphe "nom" pour chaque article
-        let nom = document.createElement("p");
-        nom.className = "nom";
-        articleCharacter.append(nom);
-        nom.innerText = ligne.name;
-        
-        // Permet de creer une div "short description" pour chaque article
-        let shortdescription = document.createElement("div");
-        shortdescription.className = "shortdescription";
-        articleCharacter.append(shortdescription);
-        shortdescription.innerText = ligne.shortDescription;
-        
-        // Permet de creer une div "description" pour chaque article
-        let description = document.createElement("div");
-        description.className = "description";
-        articleCharacter.append(description);
-        description.innerHTML = ligne.description;
+// Fonction pour créer un article
+const createArticle = (data) => {
+    const articleCharacter = document.createElement("article");
+    main.append(articleCharacter);
 
-        //permet de creer un bouton pemettant de supprimer une carte
-        let buttondelete = document.createElement("button");
-        articleCharacter.appendChild(buttondelete);
-        buttondelete.className = "buttondelete";
-        console.log(buttondelete);
-        buttondelete.innerHTML = "Delete";
-        deleteButton(ligne.id, buttondelete);
-        
-        
-        console.log(ligne);
-        // bouton permmettant de modifier une carte
-        let buttonedit = document.createElement("a");
-        articleCharacter.appendChild(buttonedit);
-        buttonedit.setAttribute('href','create.html?id='+ligne.id)
-        buttonedit.className = "buttonedit";
-        buttonedit.innerHTML = "Edit";
-                        
-        // Permet d'afficher pour tester. 
-        //console.log(imgBase);
-        console.log(img);
-    });
-})
-.catch(error => console.log("Une erreur c'est produite lors du chargement de la page"));
+    const img = document.createElement("img");
+    articleCharacter.append(img);
+    img.src = `data:image/png;base64,${data.image}`;
+
+    const nom = document.createElement("p");
+    nom.className = "nom";
+    articleCharacter.append(nom);
+    nom.innerText = data.name;
+
+    const shortdescription = document.createElement("div");
+    shortdescription.className = "shortdescription";
+    articleCharacter.append(shortdescription);
+    shortdescription.innerText = data.shortDescription;
+
+    const description = document.createElement("div");
+    description.className = "description";
+    articleCharacter.append(description);
+    description.innerHTML = data.description;
+
+    const buttondelete = document.createElement("button");
+    articleCharacter.appendChild(buttondelete);
+    buttondelete.className = "buttondelete";
+    buttondelete.innerHTML = "Delete";
+    deleteButton(data.id, buttondelete);
+
+    const buttonedit = document.createElement("a");
+    articleCharacter.appendChild(buttonedit);
+    buttonedit.href = `create.html?id=${data.id}`;
+    buttonedit.className = "buttonedit";
+    buttonedit.innerHTML = "Edit";
+};
