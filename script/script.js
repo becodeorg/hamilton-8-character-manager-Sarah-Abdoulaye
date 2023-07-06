8f8a7fbbe053ad76b1139972f2b261d217ee7e27
Sarah Bourgeois <https://github.com/Sarah2511>
2023-07-06 10:44:21 +0200

    modif barre de navigation pour être plus appropriée

--- script/script.js
+++
@@ -1,6 +0,0 @@
-const menuHamburger = document.querySelector(".burger");
-const navLinks = document.querySelector(".menu");
-//menu hamburger
-menuHamburger.addEventListener('click',()=>{
-navLinks.classList.toggle('mobile-menu')
-});
